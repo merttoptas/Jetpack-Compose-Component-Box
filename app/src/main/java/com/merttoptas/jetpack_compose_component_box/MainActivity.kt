73579e5fbb03b11740fe85df41b550871aa12ffd
Mert Toptaş <merttoptas34@gmail.com>
2022-06-16 10:11:41 +0300

    custom elevated button component added.

--- app/src/main/java/com/merttoptas/jetpack_compose_component_box/MainActivity.kt
+++ app/src/main/java/com/merttoptas/jetpack_compose_component_box/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.merttoptas.jetpack_compose_component_box
 
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.merttoptas.jetpack_compose_component_box.components.CustomElevatedButton
 import com.merttoptas.jetpack_compose_component_box.ui.theme.JetpackComposeComponentBoxTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +19,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeComponentBoxTheme {
+                val scaffoldState = rememberScaffoldState()
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize(),
+                        scaffoldState = scaffoldState,
+                        content = { Body() },
+                    )
+
                 }
             }
         }
@@ -30,14 +38,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackComposeComponentBoxTheme {
-        Greeting("Android")
+private fun Body() {
+    val content: @Composable (ColumnScope.() -> Unit) = {
+        CustomElevatedButton(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {},
+            text = "Enabled",
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color(0xffE5EADC)
+            ),
+            leadingIcon = Icons.Default.Add,
+            iconTintColor = Color(0xff648A4F),
+            textColor = Color(0xff648A4F),
+        )
     }
+
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(horizontal = 20.dp, vertical = 20.dp), content = content
+    )
 }
---
+++ app/src/main/java/com/merttoptas/jetpack_compose_component_box/components/CustomElevatedButton.kt
@@ -0,0 +1,45 @@
+package com.merttoptas.jetpack_compose_component_box.components
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CustomElevatedButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    text: String,
+    textColor: Color = Color.Black,
+    enabled: Boolean = true,
+    elevation: ButtonElevation? = ButtonDefaults.elevation(5.dp),
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    shape: RoundedCornerShape = RoundedCornerShape(50),
+    leadingIcon: ImageVector? = null,
+    iconTintColor: Color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
+) {
+
+    Button(
+        onClick = onClick,
+        modifier = modifier.height(50.dp),
+        enabled = enabled,
+        elevation = elevation,
+        colors = colors,
+        shape = shape
+    ) {
+        if (leadingIcon != null) {
+            Icon(
+                leadingIcon,
+                "menu",
+                modifier = Modifier.padding(horizontal = 5.dp),
+                tint = iconTintColor
+            )
+        }
+        Text(text = text, color = textColor)
+    }
+}
