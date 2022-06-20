1f928274bbe15bfa1ec1f26f82ba000597c19ba9
Cengiz TORU <cengiztoru@gmail.com>
2022-06-20 12:07:22 +0300

    Created Simple CircularLoadingBar

---
+++ app/src/main/java/com/merttoptas/jetpack_compose_component_box/components/loading/CircularLoadingBar.kt
@@ -0,0 +1,17 @@
+package com.merttoptas.jetpack_compose_component_box.components.loading
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CircularLoadingBar(modifier: Modifier = Modifier) {
+    CircularProgressIndicator(
+        modifier = modifier
+            .size(200.dp),
+        color = Color.Green
+    )
+}
