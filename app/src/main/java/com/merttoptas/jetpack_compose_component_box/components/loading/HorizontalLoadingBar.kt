ab10063edd7c105b3cd07183560d0982b27cbcb4
Cengiz TORU <cengiztoru@gmail.com>
2022-06-20 12:07:55 +0300

    Created Simple HorizontalLoadingBar

---
+++ app/src/main/java/com/merttoptas/jetpack_compose_component_box/components/loading/HorizontalLoadingBar.kt
@@ -0,0 +1,25 @@
+package com.merttoptas.jetpack_compose_component_box.components.loading
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HorizontalLoadingBar(modifier: Modifier = Modifier) {
+    LinearProgressIndicator(
+        modifier = modifier
+            .padding(16.dp)
+            .clip(CircleShape)
+            .fillMaxWidth()
+            .height(20.dp),
+        backgroundColor = Color.LightGray,
+        color = Color.Green
+    )
+}
