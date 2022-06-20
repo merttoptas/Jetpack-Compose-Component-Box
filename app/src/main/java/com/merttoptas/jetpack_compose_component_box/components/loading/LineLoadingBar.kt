ade52b0b94ee3bbf55c7250586e6c4706dd1d51f
Cengiz TORU <cengiztoru@gmail.com>
2022-06-20 12:08:03 +0300

    Created Thin HorizontalLoadingBar

---
+++ app/src/main/java/com/merttoptas/jetpack_compose_component_box/components/loading/LineLoadingBar.kt
@@ -0,0 +1,22 @@
+package com.merttoptas.jetpack_compose_component_box.components.loading
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ThinLoadingBar(modifier: Modifier = Modifier) {
+    LinearProgressIndicator(
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .height(2.dp),
+        backgroundColor = Color.LightGray,
+        color = Color.Green
+    )
+}
