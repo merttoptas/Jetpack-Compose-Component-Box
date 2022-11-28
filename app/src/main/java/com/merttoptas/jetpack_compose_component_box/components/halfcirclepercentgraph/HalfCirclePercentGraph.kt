0a321f3400cc0fa9caab05d1648af1539c6ff7b3
Mert Toptaş <merttoptas34@gmail.com>
2022-11-28 08:33:16 +0300

    Dependencies update

---
+++ app/src/main/java/com/merttoptas/jetpack_compose_component_box/components/halfcirclepercentgraph/HalfCirclePercentGraph.kt
@@ -0,0 +1,6 @@
+package com.merttoptas.jetpack_compose_component_box.components.halfcirclepercentgraph
+
+/**
+* Created by merttoptas on 4.09.2022.
+*/class HalfCirclePercentGraph {
+}
---
+++ app/src/main/java/com/merttoptas/jetpack_compose_component_box/screen/canvasviews/halfcirclepercentgraph/HalfCirclePercentGraphScreen.kt
@@ -0,0 +1,6 @@
+package com.merttoptas.jetpack_compose_component_box.screen.halfcirclepercentgraph
+
+/**
+* Created by merttoptas on 4.09.2022.
+*/class HalfCirclePercentGraphScreen {
+}
